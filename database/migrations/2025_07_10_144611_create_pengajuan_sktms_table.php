b74eec0c61dc9c3499387ea7692c5518bbb5274c
Galih Bagaskoro <89682863+galihbk@users.noreply.github.com>
2025-07-12 09:49:21 +0700

    Update 2025_07_10_144611_create_pengajuan_sktms_table.php

--- database/migrations/2025_07_10_144611_create_pengajuan_sktms_table.php
+++ database/migrations/2025_07_10_144611_create_pengajuan_sktms_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('pengajuan_sktms', function (Blueprint $table) {
             $table->id();
             $table->string('nik');
-            $table->string('email');
             $table->string('wa');
             $table->integer('status')->default(0);
             $table->timestamps();
